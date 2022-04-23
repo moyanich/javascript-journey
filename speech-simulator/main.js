aabdc9c80635ad920934987cbea8d0eb2e32f4a6
Amoy N <moyanich@users.noreply.github.com>
2022-04-23 17:44:22 -0500

    new files

--- speech-simulator/main.js
+++ speech-simulator/main.js
@@ -1,35 +1,15 @@
-document.querySelector('#check').addEventListener('click', check);
-
 const synth = window.speechSynthesis;
-
-
-function check() {
-      const day = document.querySelector('#day').value;
-      const schoolday = [ "Tuesday", "Thursday"]; // Using the JavaScript Array includes()
-      const weekday = ["Monday", "Wednesday", "Friday"]; // Using the JavaScript Array includes()
-      
-      if(schoolday.includes(day)) {
-            document.getElementById("placetosee").innerHTML = "You have class!";
-      } else if(weekday.includes(day)) {
-            document.getElementById("placetosee").innerHTML = "Boring Day!";
-      }  else {
-            document.getElementById("placetosee").innerHTML = "Its the Weekend";
-      }
-}
-
 
 document.querySelector('#speak').addEventListener('click', speak);
 
 function speak() {
-      const fisrtName = document.querySelector('#firstName').value;
-      const lastName = document.querySelector('#lastName').value;
+      const message = document.querySelector('#message').value;
 
-      const textToYell = `${fisrtName} ${lastName}`;
+      const textToYell = `${message}`;
 
-      document.getElementById("yell").innerText = textToYell;
+      document.getElementById("speechInterpreter").innerText = textToYell;
 
       let yellThis = new SpeechSynthesisUtterance(textToYell);
       synth.speak(yellThis);
-      
 }
 
