88620399bf147b8bb2659195e21ea003b49d89ca
Amoy N <moyanich@users.noreply.github.com>
2022-04-24 08:19:02 -0500

    Speech Simulator

--- speech-simulator/main.js
+++ speech-simulator/main.js
@@ -1,15 +1,11 @@
 const synth = window.speechSynthesis;
-
 document.querySelector('#speak').addEventListener('click', speak);
 
 function speak() {
-      const message = document.querySelector('#message').value;
-
-      const textToYell = `${message}`;
-
-      document.getElementById("speechInterpreter").innerText = textToYell;
-
-      let yellThis = new SpeechSynthesisUtterance(textToYell);
-      synth.speak(yellThis);
+	const message = document.querySelector('#message').value;
+	const textToYell = `${message}`;
+	document.getElementById("speechOutput").innerText = textToYell;
+	let yellThis = new SpeechSynthesisUtterance(textToYell);
+	synth.speak(yellThis);
 }
 
